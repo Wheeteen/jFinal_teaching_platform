2446f1a8329d57face0b407f4a86aa7c3654fb74
diang <1970234657@qq.com>
2018-01-13 22:18:29 +0800

    添加sql文件

---
+++ teaching_platform/sql/status_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO `status_type` VALUES (1, '正常');
+INSERT INTO `status_type` VALUES (2, '交换生');
+INSERT INTO `status_type` VALUES (3, '休学');
+INSERT INTO `status_type` VALUES (4, '出国');
---
+++ teaching_platform/sql/user_info.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user_info` VALUES (1, NULL, '123', 2014, 1, 1);
+INSERT INTO `user_info` VALUES (2, NULL, '123', 2015, 1, 1);
+INSERT INTO `user_info` VALUES (3, NULL, 'abc', 2011, 1, 1);
+INSERT INTO `user_info` VALUES (4, NULL, '333', 2000, 1, 1);
+INSERT INTO `user_info` VALUES (5, NULL, '56788', 2019, 1, 1);
